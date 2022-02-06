d50bbed4c4aca68138e530b2d213cae907254d70
Supergamerr194 <57563420+joseca1979@users.noreply.github.com>
2022-02-06 12:02:10 +0100

    Add files via upload

---
+++ GIT CODE.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+ using namespace std;
+ int main(){
+    string command;
+    if (command == "go") {
+      cout << "GO WHERE\n"; cin >> command;
+
+    } 
+      if (command == "east"){
+      cout << "\n"; cin >> command;
+     }
+     else if (command == "west"){
+       cout << "\n"; cin >> command;
+     } else if (command == "south"){
+       cout << "\n"; cin >> command;
+     } else if (command == "north") {
+       cout << "\n";
+     }
+ } 
